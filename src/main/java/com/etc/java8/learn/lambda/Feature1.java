337d9e684dd5ab18b117b17a59dbdde226db9e36
raodong.zhang <raodong.zhang@leyantech.com>
2021-07-28 15:15:17 +0800

    master 提交1

---
+++ src/main/java/com/etc/java8/learn/lambda/Feature1.java
@@ -0,0 +1,11 @@
+package com.etc.java8.learn.lambda;
+
+/**
+ * @author raodong.zhang, {@literal <raodong.zhang@leyantech.com>}
+ * @date 2021-07-28.
+ */
+public class Feature1 {
+
+  private String sid;
+
+}
